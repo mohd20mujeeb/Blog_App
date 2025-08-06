d7a203137ca65fcdd6007a2f429847888caa650b
mohd20mujeeb <mohd.20mujeeb@gmail.com>
2025-08-06 21:58:21 +0530

    redux store work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
+ const [loading,setLoading]=useState(true)
+ const dispatch=useDispatch()
+ 
+ useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>{setLoading(false)})
+ },[])
 
 
-  return (
-    <>
-      <h1>Assalmu Allaikum</h1>
-    </>
-  )
+ return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+  <div className='w-full block'>
+    <Header/>
+    <main>
+      todo
+    </main>
+    <Footer/>
+  </div>
+  </div>
+ ): (null)
 }
 
 export default App
