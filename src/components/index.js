f269db6026eb4c0b10a3abc833d71c2782a3adf7
mohd20mujeeb <mohd.20mujeeb@gmail.com>
2025-08-09 16:09:00 +0530

    components pages work done

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,22 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
